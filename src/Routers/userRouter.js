52b1d2880f0a9a9b9b320355d42d92fe7b16e6c6
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-07-15 17:40:38 +0900

    8.3 Edit user profile

--- src/Routers/userRouter.js
+++ src/Routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { edit, remove, startGithubLogin, finishGithubLogin, logout} from "../controllers/userController";
+import { getEdit, postEdit, startGithubLogin, finishGithubLogin, logout} from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/edit", edit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
-userRouter.get("/logout", logout);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/logout",protectorMiddleware ,logout);
 
 
 export default userRouter;
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,24 @@
 export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "WeTube";
-    res.locals.loggedInUser = req.session.user;
+    res.locals.loggedInUser = req.session.user || {};
     // locals: template과 연결 가능
     console.log(res.locals)
     next();
 }
+
+export const protectorMiddleware = (req, res, next) => {
+    if(req.session.loggedIn) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};  
+
+export const publicOnlyMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn) {
+        return next();
+    } else {
+        return res.redirect("/");
+    }
+};
