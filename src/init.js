0a288118603e2a2851acd511e444482f12cd6f33
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-08-29 01:01:29 +0900

    PORT env variable (console.log)
    variable in console.log

--- src/init.js
+++ src/init.js
@@ -8,6 +8,6 @@ import app from "./server"
 
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log("✅ Server Listening on port 4000");
+const handleListening = () => console.log(`✅ Server Listening on port ${PORT}`);
 // 서버가 port 4000을 Listening 하고 있음
 app.listen(PORT, handleListening);
